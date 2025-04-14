0346a366b67ee02269f4b6515cb657a9a0c5706c
Mohammad Aminul Islam <120117040+aminbiography@users.noreply.github.com>
2025-04-14 11:59:00 +0600

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,50 @@
 # Google Cybersecurity Professional Certificate
+
+Live URL: https://aminbiography.github.io/Google-Cybersecurity-Professional-Certificate/
+
+   
+  
+<h1>MIT License</h1>
+ 
+<h2>Copyright (c) 2024 Mohammad Aminul Islam</h2>
+
+<h3>This file is part of the Google Cybersecurity Professional Certificate program on Coursera. (Personal Short Note)</h3>
+<h4>Licensed under the MIT License. See the LICENSE file for details.</h4>
+
+<p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Materials"), to deal in the Materials without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Materials, and to permit persons to whom the Materials are furnished to do so, subject to the following conditions:</p>
+<p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Materials.</p>
+
+<p>THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.</p>
+
+--------------------------------------------------------------------------------------------------------------------
+
+
+<img src="https://coursera-certificate-images.s3.amazonaws.com/2GLRWLH1Y7SK?fbclid=IwY2xjawJpVN5leHRuA2FlbQIxMAABHpahWx1oi2NS4hAC90soeMCosmftxCfb4P4C_uqtrYrLnRsNBfmMkpIp7jGZ_aem_4RYxqSfT71W1IwXhU0p-tw" alt="Coursera Certificate" width="1200"/>
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~21J47YRQG8P5/CERTIFICATE_LANDING_PAGE~21J47YRQG8P5.jpeg)](https://www.coursera.org/account/accomplishments/certificate/21J47YRQG8P5)
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~OA7G8JR2H7Z9/CERTIFICATE_LANDING_PAGE~OA7G8JR2H7Z9.jpeg)](https://www.coursera.org/account/accomplishments/certificate/OA7G8JR2H7Z9)
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~IAC5IIN9W8WQ/CERTIFICATE_LANDING_PAGE~IAC5IIN9W8WQ.jpeg)](https://www.coursera.org/account/accomplishments/certificate/IAC5IIN9W8WQ)
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~CPA899ZQOXHC/CERTIFICATE_LANDING_PAGE~CPA899ZQOXHC.jpeg)](https://www.coursera.org/account/accomplishments/certificate/CPA899ZQOXHC)
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~77U1JUDD0QKY/CERTIFICATE_LANDING_PAGE~77U1JUDD0QKY.jpeg)](https://www.coursera.org/account/accomplishments/certificate/77U1JUDD0QKY)
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~YNXA72UPOP5S/CERTIFICATE_LANDING_PAGE~YNXA72UPOP5S.jpeg)](https://www.coursera.org/account/accomplishments/certificate/HYVE1Z9ZT2LV)
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~HYVE1Z9ZT2LV/CERTIFICATE_LANDING_PAGE~HYVE1Z9ZT2LV.jpeg)](https://www.coursera.org/account/accomplishments/certificate/HYVE1Z9ZT2LV)
+
+
+[![Coursera Certificate](https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~CIUW7RTZ0CA0/CERTIFICATE_LANDING_PAGE~CIUW7RTZ0CA0.jpeg)](https://www.coursera.org/account/accomplishments/verify/CIUW7RTZ0CA0)
+
+
+
+
