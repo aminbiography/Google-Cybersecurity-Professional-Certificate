7e6c479e46e4d3e7ee3f5d6f3b6ab8ab4e5cd127
Mohammad Aminul Islam <120117040+aminbiography@users.noreply.github.com>
2025-06-04 20:59:10 +0600

    Update LICENSE.py

--- LICENSE.py
+++ LICENSE.py
@@ -1,8 +1,8 @@
 # MIT License
 #
-# Copyright (c) 2025 Mohammad Aminul Islam
+# Copyright (c) 2024 Mohammad Aminul Islam
 #
-# This file is part of the Google Cloud Cybersecurity Professional Certificate program on Coursera. (Personal Short Note) and Certifications.
+# This file is part of the Google  Cybersecurity Professional Certificate program on Coursera. (Personal Short Note) and Certifications.
 # Licensed under the MIT License. See the LICENSE file for details.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Materials"), to deal in the Materials without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Materials, and to permit persons to whom the Materials are furnished to do so, subject to the following conditions:
